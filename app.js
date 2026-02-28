d050cb68c164f10671d3b1125e190cd08f8a84b2
rasel3980 <rkrasel3980@gmail.com>
2026-02-28 10:16:45 +0600

    added html page

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/views/index.html')
+})
+
 module.exports = app;
